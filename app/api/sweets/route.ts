e30fc74108ea79a4aa0fec26a96230b868b68459
abhishekms0109 <89069439+abhishekms0109@users.noreply.github.com>
2025-06-15 12:25:41 +0530

    Delete app directory

--- app/api/sweets/route.ts
+++
@@ -1,7 +0,0 @@
-import { getSweets } from "@/lib/sweets-service"
-import { NextResponse } from "next/server"
-
-export async function GET() {
-  const sweets = await getSweets()
-  return NextResponse.json(sweets)
-}
